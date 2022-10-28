2aa54f2f4aea5ab913487fe7f986ad1a2d781798
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-10-28 00:55:55 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  // if determinant is 0, the matrix is not invertible and if it is not 0, the matrix is invertible - tutorialspoint.com
+
+  
+  int InverseMat[2][2] = {};
+
+  int determinant = (InverseMat[0][0] * InverseMat[1][1]) - (InverseMat[1][0] * InverseMat[0][1]);
+double temp = (1/determinant);
+  
+  }
+
+
+  
+}
