78921fe4b285f251332701c284d73cb8676af3c7
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-10-28 01:24:41 +0000

    added else after the if statement and code to check each of the four indexes of the 2x2 matrix as well as a potential test case

--- main.cpp
+++ main.cpp
@@ -8,13 +8,21 @@ int main() {
   // if determinant is 0, the matrix is not invertible and if it is not 0, the matrix is invertible - tutorialspoint.com
 
   
-  int InverseMat[2][2] = {};
+  int InverseMat[2][2] = { {0, 1},
+                           {1, 0} };
 
   int determinant = (InverseMat[0][0] * InverseMat[1][1]) - (InverseMat[1][0] * InverseMat[0][1]);
 double temp = (1/determinant);
   if(determinant == 0){
     cout << "The inverse does not exist, your matrix is a singular matrix." << endl;
     }
+    else{
+      InverseMat[0][0] = temp * InverseMat[1][1];
+      InverseMat[0][1] = temp * -1 * InverseMat[0][1];
+      InverseMat[1][0] = temp * -1 * InverseMat[1][0];
+      InverseMat[1][1] = temp * InverseMat[0][0];
+
+      cout << "The inverse of your matrix exists";
 
   }
 
